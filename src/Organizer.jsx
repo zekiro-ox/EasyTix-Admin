a29b7dc587d411f1ab6993079a8e10006361c8b9
Rendelle Bertes <118584640+zekiro-ox@users.noreply.github.com>
2024-10-07 19:59:06 +0800

    few

--- src/Organizer.jsx
+++ src/Organizer.jsx
@@ -136,7 +136,7 @@ const OrganizerComponent = () => {
           )}
 
           <div className="overflow-x-auto">
-            <table className="min-w-full bg-gray-800 rounded-lg shadow-md overflow-hidden">
+            <table className="min-w-full bg-gray-700 rounded-lg shadow-md overflow-hidden">
               <thead className="bg-purple-800 text-white">
                 <tr>
                   <th className="px-6 py-3 text-left text-xs font-medium tracking-wider">
